300b6ea27803eece7330ca05de460f7711112e7c
conquerorg <gitconquerorg@gmail.com>
2023-04-27 23:58:50 +0900

    fix: I18Provider 및 RouterProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Suspense } from 'react'
+import { I18nextProvider } from 'react-i18next'
+import { RouterProvider } from 'react-router-dom'
 
-import Home from './pages/Home'
+import { i18n } from './i18n/i18n'
+import { AppRouter } from './routes'
 import { EsThemeProvider } from './theme'
 
 export const App = () => {
   return (
     <EsThemeProvider>
-      <Suspense fallback={<div>loading...</div>}>
-        <Home />
-      </Suspense>
+      <I18nextProvider i18n={i18n}>
+        <Suspense fallback={<div>loading...</div>}>
+          <RouterProvider router={AppRouter} />
+        </Suspense>
+      </I18nextProvider>
     </EsThemeProvider>
   )
 }
